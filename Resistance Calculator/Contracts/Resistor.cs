8a38249e0be1461911a0e1d5551485bf009c9344
Bradley <bradley.thomas60@googlemail.com>
2023-11-21 18:57:42 +0000

    Added favicon to project

--- Resistance Calculator/Contracts/Resistor.cs
+++ Resistance Calculator/Contracts/Resistor.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Resistance_Calculator
+﻿namespace Resistance_Calculator
 {
   public class Resistor
   {
